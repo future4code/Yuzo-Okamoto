453cf168dd07b3e55b8f36d75afb5b598d4a71f7
yuzokamoto <yuzokamoto@gmail.com>
2020-05-19 21:49:32 -0300

    removido ./build e adicionado exercício feito com o boiler-plate

---
+++ semana-6/aula-2/boiler-plate/build/precache-manifest.0816da74a5fc798382a8c7a1a3581d01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bafb9d9aa3bad3e8f2d8e334610ba95b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bec94bcc34cfd26a53be",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "28e1f6e4c00be6f90c95",
+    "url": "/static/js/2.8fdc7c9c.chunk.js"
+  },
+  {
+    "revision": "bec94bcc34cfd26a53be",
+    "url": "/static/js/main.bca25889.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
